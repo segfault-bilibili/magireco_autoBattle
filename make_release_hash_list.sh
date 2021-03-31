796616d5e339a8826043a2fff692d9b7410cd4b0
segfault-bilibili <segfault_bilibili@qq.com>
2021-03-31 18:11:25 +0800

    make_release_hash_list.sh: fix end-of-line issue when no lf in the end

--- make_release_hash_list.sh
+++ make_release_hash_list.sh
@@ -21,6 +21,20 @@ sed -i "s/\"versionName\": \"[^\"]*\"/\"versionName\": \"${NEWVERSION}\"/g" proj
 sed -i "s/var version = \"[^\"]*\"/var version = \"${NEWVERSION}\"/g" main.js
 sed -i "s/ version: '[^']*',/ version: '${NEWVERSION}',/g" floatUI.js
 
+for FILE in project.json main.js floatUI.js; do
+    sed -i 's/\r$//g' "${FILE}"
+    sed -i 's/$/\r/g' "${FILE}"
+    TRAILING=$(tail -c1 "${FILE}" | xxd -p | tr ABCDEF abcdef)
+    if [[ "${TRAILING}" == "0d" ]]; then
+        stat --format %s "${FILE}" > /dev/null || exit 1
+        FILE_SIZE=$(stat --format %s "${FILE}")
+        NEW_FILE_SIZE=$(( FILE_SIZE - 1 ))
+        mv "${FILE}" "${FILE}_trailingCR"
+        dd if="${FILE}_trailingCR" of="${FILE}" bs=${NEW_FILE_SIZE} count=1 2> /dev/null
+        rm "${FILE}_trailingCR"
+    fi
+done
+
 find . | grep -v ^\\./\\.git | grep -v ^\\.$ | grep -v ^\\./version \
 | while read line; do
     if [ -d "$line" ] ; then
