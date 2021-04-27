9c3ff7ae777d69fddd8748d46ec3ae2f325960b2
segfault-bilibili <segfault_bilibili@qq.com>
2021-04-27 19:12:34 +0800

    fix upload_to_device/emulator.cmd path

--- upload_to_device.cmd
+++ upload_to_device.cmd
@@ -13,28 +13,28 @@ for %%f in (floatUI.js main.js make_release_hash_list.sh upload_to_emulator.cmd 
 )
 
 pushd bin
-
+adb -d shell "mkdir -p /sdcard/magireco_autoBattle_mod/git-master/bin"
 for %%f in (*.*) do (
-  adb -d push %%f /sdcard/magireco_autoBattle_mod/git-master/
-  adb -d shell "su -c 'cat /sdcard/magireco_autoBattle_mod/git-master/%%f > /data/data/top.momoe.auto.segfaultmod/files/project/bin/%%f'"
+  adb -d push %%f /sdcard/magireco_autoBattle_mod/git-master/bin/
+  adb -d shell "su -c 'cat /sdcard/magireco_autoBattle_mod/git-master/bin/%%f > /data/data/top.momoe.auto.segfaultmod/files/project/bin/%%f'"
 )
 
 popd
 
 pushd images
-
+adb -d shell "mkdir -p /sdcard/magireco_autoBattle_mod/git-master/images"
 for %%f in (*.*) do (
-  adb -d push %%f /sdcard/magireco_autoBattle_mod/git-master/
-  adb -d shell "su -c 'cat /sdcard/magireco_autoBattle_mod/git-master/%%f > /data/data/top.momoe.auto.segfaultmod/files/project/images/%%f'"
+  adb -d push %%f /sdcard/magireco_autoBattle_mod/git-master/images/
+  adb -d shell "su -c 'cat /sdcard/magireco_autoBattle_mod/git-master/images/%%f > /data/data/top.momoe.auto.segfaultmod/files/project/images/%%f'"
 )
 
 popd
 
 pushd versions
-
+adb -d shell "mkdir -p /sdcard/magireco_autoBattle_mod/git-master/versions"
 for %%f in (*.txt) do (
-  adb -d push %%f /sdcard/magireco_autoBattle_mod/git-master/
-  adb -d shell "su -c 'cat /sdcard/magireco_autoBattle_mod/git-master/%%f > /data/data/top.momoe.auto.segfaultmod/files/project/versions/%%f'"
+  adb -d push %%f /sdcard/magireco_autoBattle_mod/git-master/versions/
+  adb -d shell "su -c 'cat /sdcard/magireco_autoBattle_mod/git-master/versions/%%f > /data/data/top.momoe.auto.segfaultmod/files/project/versions/%%f'"
 )
 
 popd
--- upload_to_emulator.cmd
+++ upload_to_emulator.cmd
@@ -13,28 +13,28 @@ for %%f in (floatUI.js main.js make_release_hash_list.sh upload_to_emulator.cmd 
 )
 
 pushd bin
-
+adb -e shell "mkdir -p /sdcard/magireco_autoBattle_mod/git-master/bin"
 for %%f in (*.*) do (
-  adb -e push %%f /sdcard/magireco_autoBattle_mod/git-master/
-  adb -e shell "cat /sdcard/magireco_autoBattle_mod/git-master/%%f > /data/data/top.momoe.auto.segfaultmod/files/project/bin/%%f"
+  adb -e push %%f /sdcard/magireco_autoBattle_mod/git-master/bin/
+  adb -e shell "cat /sdcard/magireco_autoBattle_mod/git-master/bin/%%f > /data/data/top.momoe.auto.segfaultmod/files/project/bin/%%f"
 )
 
 popd
 
 pushd images
-
+adb -e shell "mkdir -p /sdcard/magireco_autoBattle_mod/git-master/images"
 for %%f in (*.*) do (
-  adb -e push %%f /sdcard/magireco_autoBattle_mod/git-master/
-  adb -e shell "cat /sdcard/magireco_autoBattle_mod/git-master/%%f > /data/data/top.momoe.auto.segfaultmod/files/project/images/%%f"
+  adb -e push %%f /sdcard/magireco_autoBattle_mod/git-master/images/
+  adb -e shell "cat /sdcard/magireco_autoBattle_mod/git-master/images/%%f > /data/data/top.momoe.auto.segfaultmod/files/project/images/%%f"
 )
 
 popd
 
 pushd versions
-
+adb -e shell "mkdir -p /sdcard/magireco_autoBattle_mod/git-master/versions"
 for %%f in (*.txt) do (
-  adb -e push %%f /sdcard/magireco_autoBattle_mod/git-master/
-  adb -e shell "cat /sdcard/magireco_autoBattle_mod/git-master/%%f > /data/data/top.momoe.auto.segfaultmod/files/project/versions/%%f"
+  adb -e push %%f /sdcard/magireco_autoBattle_mod/git-master/versions/
+  adb -e shell "cat /sdcard/magireco_autoBattle_mod/git-master/versions/%%f > /data/data/top.momoe.auto.segfaultmod/files/project/versions/%%f"
 )
 
 popd
