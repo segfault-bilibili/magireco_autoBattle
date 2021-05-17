9c8bfcc9959598a022674c6c6bc476f9977327c7
segfault-bilibili <segfault_bilibili@qq.com>
2021-05-17 22:11:18 +0800

    use desc when text is null

--- floatUI.js
+++ floatUI.js
@@ -1390,7 +1390,7 @@ function algo_init() {
 
     function getContent(element) {
         if (element) {
-            return element.text() === "" ? element.desc() : element.text();
+            return (element.text() === "" || element.text() == null) ? element.desc() : element.text();
         }
     }
 
