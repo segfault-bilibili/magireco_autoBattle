20d7f7fc7c4ea64c20251caaaf02720f93f30e2b
segfault-bilibili <segfault_bilibili@qq.com>
2021-06-17 00:59:42 +0800

    支持在队伍调整界面启动默认周回脚本

--- floatUI.js
+++ floatUI.js
@@ -2060,6 +2060,7 @@ function algo_init() {
                             () => find(string.support),
                             () => findID("helpBtn"),
                             () => match(/^BATTLE.+/),
+                            () => findID("nextPageBtn"),
                         ],
                         3000
                     );
@@ -2067,6 +2068,11 @@ function algo_init() {
                     if (find(string.support)) {
                         state = STATE_SUPPORT;
                         log("进入助战选择");
+                        break;
+                    }
+                    if (findID("nextPageBtn")) {
+                        state = STATE_TEAM;
+                        log("进入队伍调整");
                         break;
                     }
 
